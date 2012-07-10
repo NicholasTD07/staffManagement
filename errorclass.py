11c7b3e9212732da121c461ea2b827a9cfa74dfe
NicholasTD07 <Nicholas.TD07@gmail.com>
2012-07-10 08:36:06 +0800

    Added license part into each file.

--- errorclass.py
+++ errorclass.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python3
 # File Name : errorclass.py
 
+#    Copyright 2012 Nicholas Tian
+
+#    This file is part of Staff Management Project.
+#
+#    Staff Management Project is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Foobar is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
 class Error(Exception) :
     """ Base class for errors in the StuffClass"""
     pass
--- groupstaffdialog.py
+++ groupstaffdialog.py
@@ -2,6 +2,23 @@
 # File Info :
 #   员工分组对话框
 
+#    Copyright 2012 Nicholas Tian
+
+#    This file is part of Staff Management Project.
+#
+#    Staff Management Project is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Foobar is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
 
 # PyQt #
 from PyQt4.QtCore import *
