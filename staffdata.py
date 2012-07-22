6e213bd0b87671c37c89c3485ca6e76eaddc3ef1
NicholasTD07 <Nicholas.TD07@gmail.com>
2012-07-22 16:39:38 +0800

    Corrected licence infomation in each file.

--- staffdata.py
+++ staffdata.py
@@ -10,13 +10,13 @@
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
-#    Foobar is distributed in the hope that it will be useful,
+#    Straff Management Project is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
 #
 #    You should have received a copy of the GNU General Public License
-#    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#    along with Straff Management Project.  If not, see <http://www.gnu.org/licenses/>.
 
 # 文件 #
 import pickle
